7d45309d886ee1a34cb313e9108dff390073982c
Marcos <marcosbauch56@gmail.com>
2020-09-24 20:36:42 -0300

    Exercicio entrega dia 07/10

---
+++ modulo2ex1.cpp
@@ -0,0 +1,28 @@
+ #include <iostream>
+ using namespace std;
+ 
+ nomes: vetor [1..10] de caracter;
+ nome: caracter
+ i: inteiro
+ encontrou: logico
+Inicio
+ para i de 1 ate 10 faca
+ escreval("Digite o ", i, "º nome")
+ leia(nomes[i])
+ fimpara
+ escreval("Agora, digte um nome a ser buscado.")
+ leia(nome)
+
+ encontrou<-falso
+ para i de 1 ate 10 faca
+ se (nomes[i] = nome) entao
+ encontrou<-verdadeiro
+ fimse
+ fimpara
+
+ se (encontrou = verdadeiro) entao
+ escreval("O nome encontra-se no vetor!")
+ senao
+ escreval("O nome não se econtra no vetor!")
+ fimse
+Fimalgoritmo
